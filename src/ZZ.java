acdf0fca4f9d6c3296115b3d63cfaa5862dd65c2
ZhengZeng <zhengzeng@oakland.edu>
2018-09-28 17:12:12 -0400

    Added Implementation ZZ.java

--- src/ZZ.java
+++ src/ZZ.java
@@ -1,7 +1,23 @@
 package com.company;
 public class BeerSong {
-public static void main(String[] args) {
+public static void main(String[] args{
+Ninety_Nine_Bottles_of_Beer();
 }
 public static void Ninety_Nine_Bottles_of_Beer() {
+int numBeer = 99;
+String bottleWord = "bottles";
+while (numBeer > 0) {
+if (numBeer == 1) {
+bottleWord = "bottle";
+}
+System.out.print(numBeer + " " + bottleWord + " of beer on the wall, ");
+System.out.print(numBeer + " " + bottleWord + " of beer take one down, pass it around, ");
+numBeer--;
+if(numBeer > 0) {
+System.out.println(numBeer + " " + bottleWord + " of beer on the wall.");
+} else {
+System.out.println("no more bottles of beer on the wall.");
+}
+}
 }
 }
